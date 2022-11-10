6e6345208ddd48c4b0b39dbf3f5ce1b739ab6e85
Master-Git-Hack <e.serna.1a.43@gmail.com>
2022-11-09 21:12:48 -0600

    excel reviwed

--- run.py
+++ run.py
@@ -1,32 +1,57 @@
 from src.modules.certificates import create as create_certificate
-from src.modules.email import request as send_email
+from src.modules.db_operations import get_workshops
+from src.modules.db_operations import update as update2DB
 from src.modules.drive import upload as upload2Drive
-from src.modules.db_operations import update as update2DB, get_workshops
-from src.utils.template_utils import end_process
+from src.modules.email import request as send_email
+from src.utils.template_utils import end_process, getXLSXinfo
+
 
 def main():
-    data = get_workshops()
-    for row in data:
+    data = getXLSXinfo()
+    # data = get_workshos()
+
+    row = data.to_dict("list")
+    nombre = row["NOMBRE"]
+    email = row["EMAIL"]
+    for idx in range(len(nombre)):
+        row = dict(
+            nombre=nombre[idx],
+            email=email[idx],
+            title="TEST",
+            year=22,
+            id_constancia=idx,
+            genero="male",
+            titulo="TEST",
+        )
         filename = create_certificate(row)
-        certificate_link = upload2Drive(filename,row['email'])
+        certificate_link = upload2Drive(filename, row["email"])
         if certificate_link != None:
-            if send_email(data = {
-                'File':f'{filename}.pdf',
-                'Email':row['email'],
-                'Link':certificate_link,
-                'Intro': f"Estimad{'o' if row['genero'] == 'male' else 'a'} {row['nombre'].upper()} ",
-                'Taller':row['titulo']
-            }):
+            if send_email(
+                data={
+                    "File": f"{filename}.pdf",
+                    "Email": "einar.serna@gmail.com",
+                    "Link": certificate_link,
+                    "Intro": f"Estimad{'o' if row['genero'] == 'male' else 'a'} {row['nombre'].upper()} ",
+                    "Taller": row["titulo"],
+                }
+            ):
                 print(f'Done for: {row["id_constancia"]}')
                 end_process(filename)
-                
-        else: print(f'Something went wrong with {row["id_constancia"]}')
+                break
 
-
+        else:
+            print(f'Something went wrong with {row["id_constancia"]}')
 
 
 def start():
-    main(int(intput('What do you want to do?\n1. Create Certificate\n2. Send Email\n3. Upload to Google Drive\n4. Update DB\n5. Get DB\n')))
+    main(
+        int(
+            intput(
+                "What do you want to do?\n1. Create Certificate\n2. Send Email\n3. Upload to Google Drive\n4. Update DB\n5. Get DB\n"
+            )
+        )
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/utils/template_utils.py
+++ src/utils/template_utils.py
@@ -1,21 +1,36 @@
-from pptx.util import Pt
-from pptx.dml.color import RGBColor
-from src.config import temPath, public_pwd_for_file
-from os import uname
-from subprocess import call
-from pandas import read_excel, isna
-from numpy import nan
-from random import randrange as rg
 from math import ceil
+from os import uname
+from random import randrange as rg
+from subprocess import call
+
+from numpy import nan
+from pandas import isna, read_excel
+from pptx.dml.color import RGBColor
+from pptx.util import Pt
+
+from src.config import public_pwd_for_file, temPath
 
 
 # function to read excel file
 def getXLSXinfo():
-    certificates = read_excel(open('src/schemas/Becas.xlsx', 'rb'), sheet_name = 'Sheet 1', converters = {'idx:int'}, index_col = [0])
-    return certificates.replace(r'^\s*$', nan, regex = True)
+    certificates = read_excel(
+        open("src/schemas/cientifico2022.xlsx", "rb"),
+        sheet_name="Hoja1",
+        converters={"Numero": str},
+        index_col=[0],
+    )
+    return certificates.replace(r"^\s*$", nan, regex=True)
 
-#function to set style for pptx
-def textStyle(insert, size = 10, isBold = False, fontName = 'Montserrat', color = RGBColor(0,0,0), alignment_center = False):
+
+# function to set style for pptx
+def textStyle(
+    insert,
+    size=10,
+    isBold=False,
+    fontName="Montserrat",
+    color=RGBColor(0, 0, 0),
+    alignment_center=False,
+):
     estilo = insert.font
     estilo.bold = isBold
     estilo.size = Pt(size)
@@ -23,96 +38,126 @@ def textStyle(insert, size = 10, isBold = False, fontName = 'Montserrat', color 
     estilo.color.rgb = color
     return insert
 
-def elements2change(nombre = 'John Doe', puesto = 'Asistente', lugar = False, title = False, year = 21):
-    #every index represent a value to change, example: at index 2 it changes name value at certificate
-    title = title.split(' ')
-    new_title = ''
-    for idx, word in enumerate(title):
-        if idx == ceil(len(title)/2):
-            new_title += f'\n'
+
+def elements2change(
+    nombre="John Doe", puesto="Asistente", lugar=False, title=False, year=21
+):
+    # every index represent a value to change, example: at index 2 it changes name value at certificate
+    if isinstance(title, str):
+        title = title.split(" ")
+    new_title = ""
+    for idx, word in enumerate(title or "ASISTENTE"):
+        if idx == ceil(len(title or "ASISTENTE") / 2):
+            new_title += f"\n"
         else:
-            new_title += f'{word} '
-    title = new_title.split('.')[0]
+            new_title += f"{word} "
+    title = new_title.split(".")[0]
     return {
-        'paragraphs' : {
-            2 : f'{nombre.upper()}',
-            3 : '',
-            4 : f'{title.upper()}',
+        "paragraphs": {
+            2: f"{nombre.upper()}",
+            3: "",
+            4: f"{title.upper()}",
         },
-        'font' :  'Calibri',
-        'color' : RGBColor(136,0,0),
-        'bold' : True,
-        'size' : 26,
-        'isFragmented' : {
-            2 : False,
-            3 : True,
-            4 : False
-        },
-        'text_fragmented':{
-            3:{
-                'paragraphs':{
-                    1 : 'Por su valiosa participación como: ', 
-                    2 : f'Asistente',
-                    3 : f' en el Congreso Internacional\n CIMPS 20{year} ',
-                    4 : "en el ",
-                    5 : f'{lugar}:\n' 
+        "font": "Calibri",
+        "color": RGBColor(136, 0, 0),
+        "bold": True,
+        "size": 26,
+        "isFragmented": {2: False, 3: True, 4: False},
+        "text_fragmented": {
+            3: {
+                "paragraphs": {
+                    1: "Por su valiosa participación como: ",
+                    2: f"Asistente",
+                    3: f" en el Congreso Internacional\n CIMPS 20{year} ",
+                    4: "en el ",
+                    5: f"{lugar}:\n",
                 },
-                'font' : 'Calibri',
-                'bold': {
-                    1 : False,
-                    2 : True,
-                    3 : False,
-                    4 : False,
-                    5 : True
-                },
-                'size' : 16,
-                'color' : RGBColor(0,0,0)
+                "font": "Calibri",
+                "bold": {1: False, 2: True, 3: False, 4: False, 5: True},
+                "size": 16,
+                "color": RGBColor(0, 0, 0),
             }
-        }
+        },
     }
+
 
 # function to transform pptx to pdf and encrypt it
 def pptx2pdf(filename, email):
     action = False
-    certificate = f'{temPath}{filename}'
+    certificate = f"{temPath}{filename}"
     match uname().sysname:
-        case 'Linux':
-            call(['libreoffice', '--headless', '--invisible', '--convert-to', 'pdf:writer_pdf_Export', '--outdir', f'{temPath}', f'{certificate}.pptx'])
+        case "Linux":
+            call(
+                [
+                    "libreoffice",
+                    "--headless",
+                    "--invisible",
+                    "--convert-to",
+                    "pdf:writer_pdf_Export",
+                    "--outdir",
+                    f"{temPath}",
+                    f"{certificate}.pptx",
+                ]
+            )
             action = True
-        case 'Darwin':
-            call(['soffice', '--headless', '--invisible', '--convert-to', 'pdf:writer_pdf_Export', '--outdir', f'{temPath}', f'{certificate}.pptx'])
+        case "Darwin":
+            call(
+                [
+                    "soffice",
+                    "--headless",
+                    "--invisible",
+                    "--convert-to",
+                    "pdf:writer_pdf_Export",
+                    "--outdir",
+                    f"{temPath}",
+                    f"{certificate}.pptx",
+                ]
+            )
             action = True
-        case 'Windows':
-            print('case not implemented')
+        case "Windows":
+            print("case not implemented")
         case _:
-            print('OS name not detected or supported')
+            print("OS name not detected or supported")
 
     if action:
-        #call(['mv', f'{certificate}.pdf', f'{certificate}-tmp.pdf'])
+        # call(['mv', f'{certificate}.pdf', f'{certificate}-tmp.pdf'])
 
-        #call(['rm', f'{certificate}.pptx'])
+        # call(['rm', f'{certificate}.pptx'])
 
-        #call(['pdftk', f'{certificate}-tmp.pdf', 'cat', 'output', f'{certificate}.pdf', 'owner_pw', f'#{public_pwd_for_file}_{email}_{rg(10)}', 'allow', 'printing'])
+        # call(['pdftk', f'{certificate}-tmp.pdf', 'cat', 'output', f'{certificate}.pdf', 'owner_pw', f'#{public_pwd_for_file}_{email}_{rg(10)}', 'allow', 'printing'])
 
-        #call(['rm', f'{certificate}-tmp.pdf'])
-        
+        # call(['rm', f'{certificate}-tmp.pdf'])
+
         return True
     else:
         return False
 
 
-def protect_pdf(certificate,email):
+def protect_pdf(certificate, email):
 
-    call(['mv', f'{certificate}.pdf', f'{certificate}-tmp.pdf'])
-    
-    call(['pdftk', f'{certificate}-tmp.pdf', 'cat', 'output', f'{certificate}.pdf', 'owner_pw', f'#{public_pwd_for_file}_{email}_{rg(10)}', 'allow', 'printing'])
+    call(["mv", f"{certificate}.pdf", f"{certificate}-tmp.pdf"])
 
-    call(['rm', f'{certificate}-tmp.pdf'])
+    call(
+        [
+            "pdftk",
+            f"{certificate}-tmp.pdf",
+            "cat",
+            "output",
+            f"{certificate}.pdf",
+            "owner_pw",
+            f"#{public_pwd_for_file}_{email}_{rg(10)}",
+            "allow",
+            "printing",
+        ]
+    )
 
-    call(['rm', f'{certificate}.pptx'])
+    call(["rm", f"{certificate}-tmp.pdf"])
+
+    call(["rm", f"{certificate}.pptx"])
     return True
 
+
 def end_process(filename):
-    certificate = f'{temPath}{filename}'
-    call(['rm', f'{certificate}.pdf'])
+    certificate = f"{temPath}{filename}"
+    call(["rm", f"{certificate}.pdf"])
     return True
